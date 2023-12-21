f98338a3fac5c84f3add2ba5a67b1938003dab08
anikdev711 <anikdev100@gmail.com>
2023-12-21 22:43:18 +0600

    to do list added

--- src/pages/ToDoList/ToDoList.jsx
+++ src/pages/ToDoList/ToDoList.jsx
@@ -1,9 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import axios from "axios";
 
 
 const ToDoList = () => {
+    const { user } = useContext(AuthContext);
+
+    const [toDoTasks, setToDoTasks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/tasks')
+            .then(res => {
+                const filteredTasks = res.data.filter(item => item.email === user?.email);
+                // console.log(filteredTasks);
+                setToDoTasks(filteredTasks)
+            })
+    }, [user?.email])
+
+    console.log(toDoTasks);
+
+
+
+
     return (
         <div>
-            To do lists.
+            <h1 className="text-center font-bold text-2xl">Create Task</h1>
+            <br />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mx-auto">
+                {
+                    toDoTasks.map(task => (
+                        <div key={task._id}>
+                            <div className="card bg-base-100 shadow-xl">
+                                
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {task.title}
+                                        <div className="badge badge-secondary">{task.priority}</div>
+                                    </h2>
+                                    <p>{task.description}</p>
+                                    <p>{task.deadlines}</p>
+                                    <div className="card-actions justify-start">
+                                        <div className="badge badge-outline">ongoing</div>
+                                        <div className="badge badge-outline">completed</div>
+                                        <div className="badge badge-outline">edit</div>
+                                        <div className="badge badge-outline">delete</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
